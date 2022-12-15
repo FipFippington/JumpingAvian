7e192c978a3c290e4a167fae85002811bc3e44a6
FipFippington <116121197+FipFippington@users.noreply.github.com>
2022-12-15 15:30:28 -0800

    Added Text
    Added a script, animations, physics, and text

---
+++ JumpingAvianValenzuela/Assets/Scripts/Avian.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class Avian : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public float  upForce = 200f;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+            {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        anim.SetTrigger("Die");
+        isDead = true;
+    }
+}
